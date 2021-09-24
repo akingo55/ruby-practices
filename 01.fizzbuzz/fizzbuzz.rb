f36066bdbb772fc54afc751d8e0004797785af89
akingo55 <am1157154@gmail.com>
2021-09-24 23:48:58 +0900

    create fizzbuzz program

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def fizzbuzz(num)
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
+end
+
+
+[*1..20].each do |num|
+  fizzbuzz(num)
+end
