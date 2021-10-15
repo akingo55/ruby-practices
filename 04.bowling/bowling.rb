86432e88fdf0896f2049c430817e3df15a5572a6
akingo55 <am1157154@gmail.com>
2021-10-15 20:08:41 +0900

    update the code in the case of number of count 9

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -30,17 +30,11 @@ end
 final_scores = []
 
 scores.each.with_index(1) do |score, i|
-  sum = if i >= 1 && i <= 8
-          if score[0] == 10
-            scores[i][0] == 10 ? score.sum + scores[i][0] + scores[i + 1][0] : score.sum + scores[i][0] + scores[i][1]
-          elsif score.sum == 10
-            score.sum + scores[i][0]
-          else
-            score.sum
-          end
-        elsif i == 9
-          if score[0] == 10
+  sum = if i >= 1 && i <= 9
+          if score[0] == 10 && i == 9
             score.sum + scores[i][0] + scores[i][1]
+          elsif score[0] == 10 && i != 9
+            scores[i][0] == 10 ? score.sum + scores[i][0] + scores[i + 1][0] : score.sum + scores[i][0] + scores[i][1]
           elsif score.sum == 10
             score.sum + scores[i][0]
           else
