9a738881e52e83de7dae86508419f7f37bcc77d4
akingo55 <am1157154@gmail.com>
2021-10-20 23:59:32 +0900

    add ls file

---
+++ 05.ls/ls_command.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# obtain the path of current directory
+current_dir = Dir.pwd
+
+def get_files_and_dirs(current_dir)
+  list = []
+  Dir.open(current_dir).each_child{|f|
+    unless f.start_with?('.')
+      list << f
+    end
+  }
+  return list
+end
+
+#number_of_elements = list.size
+
+# 複数行に分けて標準出力する
+def format_list(list)
+  number_of_elements = list.size
+  max_row = 3
+  if number_of_elements <= max_row
+    puts list.join("\t")
+  else
+    max_line = number_of_elements / max_row
+  end
+
+end
+
+list = get_files_and_dirs(current_dir)
+format_list(list)
