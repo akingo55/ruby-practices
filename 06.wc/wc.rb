88e47b96f25b7793e0bb38a057b37d2f3ee62cc2
akingo55 <am1157154@gmail.com>
2022-02-01 00:53:24 +0900

    modify wc

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,41 +8,47 @@ opts = OptionParser.new
 opts.on('-l') { params[:l] = true }
 opts.parse!(ARGV)
 
-def get_file_info(files)
-    return nil if files.size == 0
+def get_file_info(files, params)
+  raise 'wc: No arguments' if files.empty?
 
-    ary = []
+  ary = []
+
+  if params[:l]
     files.each do |file|
-        raise "wc: #{file}: open: No such file" unless File.file?(file)
+      raise "wc: #{file}: open: No such file" unless File.file?(file)
 
-        f = File.read(file)
-        ary << [f.lines.count, f.split(' ').size, f.size, file]
+      f = File.read(file)
+      ary << [f.lines.count, file]
     end
-    return ary
-end
-
-def get_file_info_with_l(files)
-    return nil if files.size == 0
-
-    ary = []
+  else
     files.each do |file|
-        raise "wc: #{file}: open: No such file" unless File.file?(file)
+      raise "wc: #{file}: open: No such file" unless File.file?(file)
 
-        f = File.read(file)
-        ary << [f.lines.count, file]
+      f = File.read(file)
+      ary << [f.lines.count, f.split(' ').size, f.size, file]
     end
-    return ary
+  end
+  outputs_result(ary)
 end
 
 def outputs_result(results)
-    results.each do |result|
-        puts results.join("\t")
-    end
+  results.each do |result|
+    puts result.join("\t")
+  end
 end
 
 def exec_wc(params)
-    results =  params[:l] ? get_file_info_with_l(ARGV) : get_file_info(ARGV)
-    outputs_result(results)
+  File.pipe?($stdin) ? get_info_from_standard_input(params) : get_file_info(ARGV, params)
+end
+
+def get_info_from_standard_input(params)
+  str = ARGF.readlines
+
+  if params[:l]
+    puts str.size
+  else
+    puts [str.size, str.size, str.join('').bytesize].join("\t")
+  end
 end
 
 exec_wc(params)
