04a5d7f4f90cb8fc4f41349ebd236f33e8c05e48
akingo55 <am1157154@gmail.com>
2022-02-13 19:21:17 +0900

    merge method

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,7 +8,7 @@ opts = OptionParser.new
 opts.on('-l') { params[:l] = true }
 opts.parse!(ARGV)
 
-def get_file_info(files)
+def get_file_info(files, params)
   raise 'wc: No arguments' if files.empty?
 
   ary = []
@@ -20,32 +20,17 @@ def get_file_info(files)
     raise "wc: #{file}: open: No such file" unless File.file?(file)
 
     f = File.read(file)
-    ary << [f.lines.count, f.split(' ').size, f.size, file]
-    next unless files.size > 1
+    data = params[:l] ? [f.lines.count, file] : [f.lines.count, f.split(' ').size, f.size, file]
+    ary << data
+    next if files.size <= 1
 
     total_line_count += f.lines.count
     total_size = f.split(' ').size
     total_bytesize = f.size
   end
-  ary.push([total_line_count, total_size, total_bytesize, 'total']) if files.size > 1
-  outputs_result(ary)
-end
 
-def get_file_info_with_l_option(files)
-  raise 'wc: No arguments' if files.empty?
-
-  ary = []
-  total_line_count = 0
-  files.each do |file|
-    raise "wc: #{file}: open: No such file" unless File.file?(file)
-
-    f = File.read(file)
-    ary << [f.lines.count, file]
-    next unless files.size > 1
-
-    total_line_count += f.lines.count
-  end
-  ary.push([total_line_count, 'total']) if files.size > 1
+  total = params[:l] ? [total_line_count, 'total'] : [total_line_count, total_size, total_bytesize, 'total']
+  ary.push(total) if files.size > 1
   outputs_result(ary)
 end
 
@@ -58,10 +43,8 @@ end
 def exec_wc(params)
   if File.pipe?($stdin)
     get_info_from_standard_input(params)
-  elsif params[:l]
-    get_file_info_with_l_option(ARGV)
   else
-    get_file_info(ARGV)
+    get_file_info(ARGV, params)
   end
 end
 
