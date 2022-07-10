938c6c22af7681a8eef98727baadd3056a121899
akingo55 <am1157154@gmail.com>
2022-07-10 15:38:13 +0900

    update frame class

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 class Frame
-  attr_reader :scores
+  attr_reader :score
 
-  def initialize(score_data)
-    sliced_score_data = score_data.each_slice(2).to_a
-    last_frame_scores = sliced_score_data[9..].flat_map { |s| s.grep_v(0) }
-    @scores = if last_frame_scores.size == 1
-                sliced_score_data[0..8] + [last_frame_scores << 0]
-              else
-                sliced_score_data[0..8] + [last_frame_scores]
-              end
+  def initialize(score)
+    @score = score
   end
 
-  def score(order)
-    @scores[order - 1]
+  def strike?
+    @score[0] == 10
+  end
+
+  def spare?
+    @score[0] + @score[1] == 10 && @score[0] != 10
   end
 end
