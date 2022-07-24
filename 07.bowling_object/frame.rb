d70b5bab5f729ccdee91cc617a2f2fadb4108421
akingo55 <am1157154@gmail.com>
2022-07-25 00:50:32 +0900

    update frame

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,19 +1,30 @@
 # frozen_string_literal: true
 
 class Frame
-  def initialize(scores)
-    @scores = scores
+  def initialize(first_shot, second_shot = 0, third_shot = nil)
+    @first_shot = first_shot
+    @second_shot = second_shot
+    @third_shot = third_shot
   end
 
   def strike?
-    @scores[0] == 10
+    @first_shot == 10
+  end
+
+  def bonus_score
+    if @third_shot.nil?
+      strike? ? [@first_shot] : [@first_shot, @second_shot]
+    else
+      [@first_shot, @second_shot, @third_shot]
+    end
   end
 
   def spare?
-    @scores[0..1].sum == 10 && !strike?
+    @first_shot + @second_shot == 10 && !strike?
   end
 
   def total_score
-    @scores.sum
+    @third_shot = 0 if @third_shot.nil?
+    @first_shot + @second_shot + @third_shot
   end
 end
