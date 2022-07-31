70b7c0cdfab453fc5f3ad7fb5c3700d08b8ca897
akingo55 <am1157154@gmail.com>
2022-07-31 16:40:56 +0900

    fix variable name & index

--- 07.bowling_object/main.rb
+++ 07.bowling_object/main.rb
@@ -5,31 +5,31 @@ require_relative 'frame'
 
 def main(marks)
   shots = Shot.prepare(marks)
-  frame_scores = []
+  frame_shots = []
   frames = []
 
   shots.each do |shot|
-    frame_scores << shot
+    frame_shots << shot
 
     next if frames.size >= 9
 
-    if shot.strike? && frame_scores.size != 2
-      frame_scores << Shot.new(0)
-      frames << Frame.new(*frame_scores)
-      frame_scores = []
-    elsif frame_scores.size == 2
-      frames << Frame.new(*frame_scores)
-      frame_scores = []
+    if shot.strike? && frame_shots.size != 2
+      frame_shots << Shot.new(0)
+      frames << Frame.new(*frame_shots)
+      frame_shots = []
+    elsif frame_shots.size == 2
+      frames << Frame.new(*frame_shots)
+      frame_shots = []
     end
   end
-  frames << Frame.new(*frame_scores)
+  frames << Frame.new(*frame_shots)
   puts game_score(frames)
 end
 
 def game_score(frames)
-  frames.each.with_index(1).sum do |frame, index|
-    next_frame = index == 10 ? nil : frames[index]
-    after_next_frame = index >= 9 ? nil : frames[index + 1]
+  frames.each.with_index.sum do |frame, index|
+    next_frame = index == 9 ? nil : frames[index + 1]
+    after_next_frame = index >= 8 ? nil : frames[index + 2]
     frame.total_score(next_frame, after_next_frame)
   end
 end
